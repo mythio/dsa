b9b827ed8a051c8d3377c0a5ca8b76c702c89da1
mythio <mythio.2909@gmail.com>
2019-08-09 02:37:03 +0530

    MAT08 Mean and median

--- mathematics/08_mean_and_median.cpp
+++ mathematics/08_mean_and_median.cpp
@@ -1,23 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isPrime(int n) {
-  for (int i = 2; i <= sqrt(n); ++i) {
-    if (n % i == 0) {
-      return false;
-    }
-  }
-  return true;
-}
-
-bool isExact3Div(int n) {
-  int p = sqrt(n);
-  if (n == p * p) {
-    return true;
-  }
-  return false;
-}
-
 int main(int argc, char const *argv[]) {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
