6d8e959cf3570a6f90c3f8c6bd6e4735718934c3
mythio <mythio.2909@gmail.com>
2019-08-09 02:42:09 +0530

    MAT10 Mutliplication under modulo

---
+++ mathematics/10_multiplication_under_modulo.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    long long a, b;
+    cin >> a >> b;
+    cout << ((a % mod) * (b % mod)) % mod << '\n';
+  }
+  return 0;
+}
