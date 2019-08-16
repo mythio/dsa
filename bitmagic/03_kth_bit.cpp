964c537bc327158689ec6b476aedeef7af84d253
mythio <mythio.2909@gmail.com>
2019-08-17 03:40:10 +0530

    BIT03 Kth bit

---
+++ bitmagic/03_kth_bit.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k;
+    cin >> n >> k;
+    cout << ((n & (1 << k)) ? "Yes" : "No") << '\n';
+  }
+  return 0;
+}
