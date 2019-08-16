98b364c814eb596d192009ead03c9899d6561586
mythio <mythio.2909@gmail.com>
2019-08-17 03:20:41 +0530

    BIT01 First set bit

---
+++ bitmagic/01_first_set_bit.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << max(0.0, log2((n ^ (n - 1)) + 1)) << '\n';
+  }
+  return 0;
+}
