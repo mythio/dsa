ddebfa63315cd5ccd4752ac276275e1a0c0759ae
mythio <mythio.2909@gmail.com>
2019-08-24 12:42:37 +0530

    DIP01 Move zeros

---
+++ dip/01_move_zeros.cpp
@@ -0,0 +1,41 @@
+/*
+Given an array nums, write a function to move all 0's to the end of it while
+maintaining the relative order of the non-zero elements.
+
+Example:
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> a[i];
+    }
+    int c = 0;
+    for (int i = 0; i < n; ++i) {
+      if (a[i] != 0) {
+        swap(a[i], a[c++]);
+      }
+    }
+    for (auto i : a) {
+      cout << i << ' ';
+    }
+    cout << '\n';
+  }
+  return 0;
+}
